c2f07ef1ea52e3208b5edb0ccf9e1f6f6c96abda
yrmkishore <yramkishore007@gmail.com>
2024-01-27 12:24:43 +0000

    IPL Dashboard App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Route, Switch} from 'react-router-dom'
+
+import Home from './components/Home'
+import TeamMatches from './components/TeamMatches'
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/team-matches/:id" component={TeamMatches} />
+  </Switch>
+)
+
+export default App
